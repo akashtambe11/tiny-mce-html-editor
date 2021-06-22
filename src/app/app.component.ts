f1cf688cb4b68b33b4507e438af5b107459fae40
akash Tambe <akashtambe11@gmail.com>
2021-06-22 19:01:13 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
-
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { debounceTime } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'tinyMCE';
+
+  emailTemplate: any;
+
+  ngOnInit() {
+
+  }
+
+  captureHtmlTemplate(e: any) {
+    console.log(e.editor.getContent());
+    this.emailTemplate = e.editor.getContent();
+
+  }
+
 }
+
